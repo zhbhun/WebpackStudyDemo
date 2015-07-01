e07b0174fe2ecb464054833657126a1d213788b5
zhanghuabin <zhanghuabin@apexsoft.com.cn>
2015-07-01 14:51:54 +0800

    Revert "Revert "Revert "Revert "代码拆分后的重复模块删除测试""""
    This reverts commit dbbc33a2fd38e9c80cfde338cafbe852f3ba0a4c.

---
+++ code-splitted-dedupe/dist1/2.bundle.js
@@ -0,0 +1,34 @@
+webpackJsonp([2],[
+/* 0 */,
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = {"this is": "z"};
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = {
+		x: __webpack_require__(6),
+		y: __webpack_require__(7),
+		z: __webpack_require__(4)
+	}
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	module.exports = {"this is": "x"};
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	module.exports = {"this is": "y", "but in": "b"};
+
+/***/ }
+]);
