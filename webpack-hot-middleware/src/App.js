2cf64e07b573fbc54cc4a2f2836af0b75c3241df
zhbhun <zhbhun@gmail.com>
2015-11-12 17:51:08 +0800

    增加 webpack-hot-middleware 使用示例

---
+++ webpack-hot-middleware/src/App.js
@@ -0,0 +1,9 @@
+import styles from './App.css';
+import React from 'react';
+
+export default class App extends React.Component {
+
+  render() {
+    return <h1 className={styles.header}>Hello world!</h1>;
+    }
+}
