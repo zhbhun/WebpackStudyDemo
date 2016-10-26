a5b769e98567c9f6b7a36dc9e2c40cc6ce8a5938
zhanghuabin <zhanghuabin@apexsoft.com.cn>
2016-10-26 16:31:44 +0800

    react-hot-boilerplate-next

---
+++ hot-module-replacement/react-hot-boilerplate-next/src/index.js
@@ -0,0 +1,34 @@
+import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const rootEl = document.getElementById('root');
+try {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    rootEl
+  );
+} catch (e) {
+  console.error(e.stack);
+}
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    // If you use Webpack 2 in ES modules mode, you can
+    // use <App /> here rather than require() a <NextApp />.
+    const NextApp = require('./App').default;
+    try {
+      ReactDOM.render(
+        <AppContainer>
+           <NextApp />
+        </AppContainer>,
+        rootEl
+      );
+    } catch (e) {
+      console.error(e.stack);
+    }
+  });
+}
