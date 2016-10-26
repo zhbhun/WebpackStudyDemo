21b618b4672d7c8d12b0f46445aa4e852dbb2244
zhanghuabin <zhanghuabin@apexsoft.com.cn>
2016-10-27 00:18:51 +0800

    add react-hot-vs-react-transform

---
+++ hot-module-replacement/react-hot-vs-react-transform/webpack.config.transform.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  // or devtool: 'eval' to debug issues with compiled output:
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    // necessary for hot reloading with IE:
+    'eventsource-polyfill',
+    // listen to code updates emitted by hot middleware:
+    'webpack-hot-middleware/client',
+    // your code:
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('transform'),
+      },
+    }),
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css?$/,
+      loaders: ['style', 'raw'],
+      include: __dirname,
+    }]
+  }
+};
